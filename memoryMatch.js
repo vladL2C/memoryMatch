5fc37641f0980ad9e64c24dd079e39081c9e8cca
Vlad <Vlad@hotmail.co.nz>
2017-12-29 21:32:54 +1300

    part 4 left

--- memoryMatch.js
+++ memoryMatch.js
@@ -67,6 +67,7 @@ function setUp(){
     });
 
     cell.addEventListener('click',function(){
+    	if(!ready) return;
     	startTimer();
     	if(this.clicked == false && this.completed == false){
     		clickedArray.push(this);
@@ -90,11 +91,18 @@ function setUp(){
             }
            else{
                 //if a matching pair is not found
-
+           	ready = false;
+           	document.getElementById("gridTable").style.border = "5px solid red";       
 
                 setTimeout(function(){
                     //after a 500ms delay
+                	hide(clickedArray[0]);
+									hide(clickedArray[1]);
 
+									clickedArray = [];
+
+									ready = true;
+									document.getElementById("gridTable").style.border = "5px solid black";    
 
                 },500);
 
